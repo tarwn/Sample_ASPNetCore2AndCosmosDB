3c6b4954e12768b58e211fcbfbcf9307d2bcdb67
Eli Weinstock-Herman <tarwn@tiernok.com>
2018-03-28 18:59:16 -0400

    Initial Commit
    ASP.Net Core 2 project with working MVC endpoint, README, and .gitignore

---
+++ SampleCosmosCore2App/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleCosmosCore2App.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
